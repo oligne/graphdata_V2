68770087be2430192c4bf77516c8894cd801d4a1
Oligne <coline.jousset@ensaama.net>
2026-03-15 23:39:18 +0100

    update

--- src/forces.js
+++ src/forces.js
@@ -1,4 +1,4 @@
-import * as d3 from 'd3';
+import { forceCenter } from 'd3-force-3d';
 
 export function applyYearForces(graph, nodes) {
   if (!graph || typeof graph.d3Force !== 'function') return;
@@ -7,7 +7,7 @@ export function applyYearForces(graph, nodes) {
     //.d3Force('x', d3.forceX(node => years.indexOf(node.group) * 200))
     //.d3Force('y', d3.forceY(() => Math.random() * 400 - 200))
     //.d3Force('z', d3.forceZ(() => Math.random() * 400 - 200))
-    .d3Force('center', d3.forceCenter(0, 0, 0))
+    .d3Force('center', forceCenter(0, 0, 0))
     .d3Force('charge').strength(-20)
    // .d3Force('link').distance(200);
 }
