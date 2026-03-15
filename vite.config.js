f1d038fe1381e081eb58f34754d3da1a9152c73b
Oligne <coline.jousset@ensaama.net>
2026-03-15 23:58:56 +0100

    fix: update vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/graphdata_V2/',
+  optimizeDeps: {
+    include: ['3d-force-graph']
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
